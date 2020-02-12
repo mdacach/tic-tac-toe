889bd4724ac49d3eadf099478a6e0309b1e0522f
matheus <dacachmatheus@gmail.com>
2020-02-12 15:49:41 -0300

    start implementing minimax

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
 const gameBoard = (() => {
     const container = document.querySelector('.container');
 
+    let divsGrid = []; 
 
-    let cellsGrid = [];
-    // private functions 
-    function constructGrid(){
-    // construct the cells and put them in a 2D array
-        for (let row = 0; row < 3; row++){
-            let elts = [];
-            for (let col = 0; col < 3; col++){
-                const div = document.createElement('div');
-                div.classList.add('cell');
-                container.appendChild(div);
-                elts.push(div);
-            }
-            cellsGrid.push(elts);
+    for (let i = 0; i < 3; i++){
+        let row = []; 
+        for (let j = 0; j < 3; j++){
+            const div = document.createElement('div');
+            div.classList.add('cell');
+            container.appendChild(div);
+            row.push(div);
         }
-    };    
+        divsGrid.push(row); 
+    }
 
 
     function addClickFunctions(cell) {
@@ -35,17 +31,16 @@ const gameBoard = (() => {
 
     // public functions 
     function start() {
-        constructGrid();
         for (let i = 0; i < 3; i++){
             for (let j = 0; j < 3; j++){
-                addClickFunctions(cellsGrid[i][j]);
+                addClickFunctions(divsGrid[i][j]);
             }
         }
     };
 
     return {
         container,
-        cellsGrid,
+        divsGrid,
         start,
     };
 })();
@@ -103,7 +98,7 @@ const gameFlow = (() => {
         function isActive (cell) {
             return cell.classList.contains('active');
         }
-        if(gameBoard.cellsGrid.every((array) => array.every(isActive))){
+        if(gameBoard.divsGrid.every((array) => array.every(isActive))){
             winner = 'draw';
             return true;
         };
@@ -130,7 +125,7 @@ const gameFlow = (() => {
 
         // check rows 
         for (let row = 0; row < 3; row++){
-            let currentRow = gameBoard.cellsGrid[row];
+            let currentRow = gameBoard.divsGrid[row];
             if (checkWinner (currentRow)) {
                 winner = checkWinner(currentRow);
                 addWinnerClass(currentRow, winner);
@@ -142,7 +137,7 @@ const gameFlow = (() => {
         for (let col = 0; col < 3; col++){
             let currentCol = [];
             for (let row = 0; row < 3; row++){
-                currentCol.push(gameBoard.cellsGrid[row][col]);
+                currentCol.push(gameBoard.divsGrid[row][col]);
             }
             if (checkWinner (currentCol)) {
                 winner = checkWinner(currentCol);
@@ -154,7 +149,7 @@ const gameFlow = (() => {
         // check principal diag
         let principalDiag = [];
         for (let diag = 0; diag < 3; diag++){
-            principalDiag.push(gameBoard.cellsGrid[diag][diag]);
+            principalDiag.push(gameBoard.divsGrid[diag][diag]);
         }
         if (checkWinner(principalDiag)) {
             winner = checkWinner(principalDiag);
@@ -165,7 +160,7 @@ const gameFlow = (() => {
         // check secondary diag
         let secondaryDiag = [];
         for (let row = 0; row < 3; row++){
-            secondaryDiag.push(gameBoard.cellsGrid[row][2 - row]);
+            secondaryDiag.push(gameBoard.divsGrid[row][2 - row]);
         }
         if (checkWinner(secondaryDiag)) {
             winner = checkWinner(secondaryDiag);
@@ -218,7 +213,6 @@ const gameFlow = (() => {
             gameFlow.turn = 'x';
 
 
-
     };
 
     return {
@@ -233,12 +227,157 @@ gameBoard.start();
 
 const computerModule = (() => {
 
-    function getAvailable() {
+    // we want a copy of the cells grid to not change anything with the minimax calculations
+    let copyCells = gameBoard.divsGrid.slice();
+
+    // we get all the available plays
+    let possiblePlays = 0;
+
+    let boardArr = [['', '', ''],
+                    ['', '', ''], 
+                    ['', '', '']];
+
+    // update board array works globally
+    function updateBoardArr(){
+
+        for (let i = 0; i < 3; i++){
+            for (let j = 0; j < 3; j++){
+                if (gameBoard.divsGrid[i][j].classList.contains('x-cell')){
+                    boardArr[i][j] = 'x';
+                }
+                if (gameBoard.divsGrid[i][j].classList.contains('o-cell')){
+                    boardArr[i][j] = 'o'; 
+                }
+            }
+        }
+    };
+
+    function minimax (board = boardArr, player = 'x') {
+        updateBoardArr(); 
+        console.log('minimaxing with board')
+        console.table(board)
+        console.log('player ', player)
+
+        let full = true; 
+        for (let i = 0; i < 3; i++) {
+            console.log(board[i]);
+            // console.log(board[i].some());
+            if (board[i].some((elt) => elt === '')){
+                full = false; 
+            }
+        }
+
+        if (full){
+            console.log('minimax terminated');
+            return getValueOfBoard(board);
+        }
+
+        
+        let availableIndexes = []; 
+        let possibleValues = []; 
+        for (let i = 0; i < 3; i++){
+            for (let j = 0; j < 3; j++){
+                if (board[i][j] === ''){
+                    // available spots 
+                    availableIndexes.push([i, j]); 
+                    // let boardCopy = board.slice(); 
+                    // boardCopy[i][j] = player; //play on that spot
+                    // if (player === 'x') player = 'o';
+                    // else player = 'x'; // change players
+
+                    // // and evaluate the new board 
+                    // return minimax(boardCopy, player); 
+                    // boardCopy[i][j] = '';
+                }
+            }
+        }
+        let allValues = [] ; 
+        for (i = 0; i < availableIndexes.length; i++){
+            let row = availableIndexes[i][0];
+            let col = availableIndexes[i][1];
+            board[row][col] = player; 
+            if (player === 'x') player = 'o';
+            else player = 'x'; // change players
+            console.log('minimaxing with board:');
+            console.table(board);
+            allValues.push(minimax(board, player));
+
+            board[row][col] = ''; 
+        }
+
+
+        // if (player === 'x') return Math.min(allValues); 
+        // else return Math.max(allValues); 
+        console.log('possible values for the plays: ', possibleValues);
+    }
+
+    function getValueOfBoard(board) {
+        /*
+        check rows, columns and diagonals for a winner
+        will return 1 if computer wins, 0 for a draw and -1 for computer loses
+        */
+        
+        // check rows 
+        for (let row = 0; row < 3; row++){
+            let currentRow = board[row];
+            if (currentRow.every((elt) => elt === 'x')) return -1;
+            if (currentRow.every((elt) => elt === 'o')) return 1;
+        }
+
+        // check cols 
+        for (let col = 0; col < 3; col++){
+            let currentCol = [];
+            for (let row = 0; row < 3; row++){
+                currentCol.push(board[row][col]);
+            }
+            if (currentCol.every((elt) => elt === 'x')) return -1;
+            if (currentCol.every((elt) => elt === 'o')) return 1;
+        }
+
+        // check principal diag
+        let principalDiag = [];
+        for (let diag = 0; diag < 3; diag++){
+            principalDiag.push(board[diag][diag]);
+        }
+        if (principalDiag.every((elt) => elt === 'x')) return -1; 
+        if (principalDiag.every((elt) => elt === 'o')) return 1; 
+    
+        // check secondary diag
+        let secondaryDiag = [];
+        for (let row = 0; row < 3; row++){
+            secondaryDiag.push(board[row][2 - row]);
+        }
+
+        if (secondaryDiag.every((elt) => elt === 'x')) return -1; 
+        if (secondaryDiag.every((elt) => elt === 'o')) return 1; 
+
+        return 0; 
+
+    }
+    // function getValue()
+
+    function getAvailable(type = '') {
+        if (type === 'copy'){
+            let available = [];
+            
+            for (let i = 0; i < 3; i++){
+                for (let j = 0; j < 3; j++){
+                    if (!copyCells[i][j].classList.contains('active')){
+                        available.push(copyCells[i][j]);
+                    }
+                }
+            }
+
+            return available; 
+
+        }
+
+
         let available = [];
         for (let i = 0; i < 3; i++){
             for (let j = 0; j < 3; j++){
-                if (!gameBoard.cellsGrid[i][j].classList.contains('active')){
-                    available.push(gameBoard.cellsGrid[i][j]);
+                if (!gameBoard.divsGrid[i][j].classList.contains('active')){
+                    available.push(gameBoard.divsGrid[i][j]);
                 }
             }
         }
@@ -264,7 +403,10 @@ const computerModule = (() => {
     }
 
     return {
+        updateBoardArr,
+        boardArr,
         play,
+        minimax, 
     };
 
 })();
